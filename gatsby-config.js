966e9ea687266df65209edfb0fcddb9b3eb0567e
Bryan Oyloe <boyloe@gmail.com>
2021-01-18 11:17:47 -0700

    Add header and text to index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby.js is the Coolest`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
